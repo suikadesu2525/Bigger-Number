93415c1ddd9a962769c87159b5a405b080a4f3dc
suikadesu2525 <1827040@brunel.ac.uk>
2019-10-17 14:32:37 +0100

    Added score functionality, fixed some UI layouts.

--- app/src/main/java/com/example/biggernumber/MainActivity.java
+++ app/src/main/java/com/example/biggernumber/MainActivity.java
@@ -12,28 +12,29 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private int userNumber1;
     private int userNumber2;
+    private int userPoints = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button userButton = findViewById(R.id.userSelect);
         final Button userButton2 = findViewById(R.id.userSelect2);
-        final TextView userOption = findViewById(R.id.displayNumber);
-        final TextView userOption2 = findViewById(R.id.displayNumber2);
+        final TextView userOption = findViewById(R.id.randomNumber);
+        final TextView userOption2 = findViewById(R.id.randomNumber2);
         final TextView selectResult = findViewById(R.id.selectResult);
+        final TextView userScore = findViewById(R.id.userScore);
 
         generateRandom();
         userOption.setText(String.valueOf(userNumber1));
         userOption2.setText(String.valueOf(userNumber2));
 
-
         userButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
 
                 if (checkOption(userNumber1, userNumber2) == true) {
                     selectResult.setText("Congratulations!");
+                    userPoints++;
                 }
                 else {
                     selectResult.setText("Too bad, try again!");
@@ -41,13 +42,16 @@ public class MainActivity extends AppCompatActivity {
                 generateRandom();
                 userOption.setText(String.valueOf(userNumber1));
                 userOption2.setText(String.valueOf(userNumber2));
+                userScore.setText(String.valueOf(userPoints));
             }
         });
+
         userButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (checkOption(userNumber2, userNumber1) == true) {
                     selectResult.setText("Congratulations!");
+                    userPoints++;
                 }
                 else {
                     selectResult.setText("Too bad, try again!");
@@ -55,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 generateRandom();
                 userOption.setText(String.valueOf(userNumber1));
                 userOption2.setText(String.valueOf(userNumber2));
+                userScore.setText(String.valueOf(userPoints));
             }
         });
     }
@@ -64,11 +69,11 @@ public class MainActivity extends AppCompatActivity {
 
             userNumber1 = randomNum.nextInt(100);
             userNumber2 = randomNum.nextInt(100);
-        while (userNumber1 == userNumber2) {
-            userNumber1 = randomNum.nextInt(100);
-            userNumber2 = randomNum.nextInt(100);
-            break;
-        }
+                while (userNumber1 == userNumber2) {
+                    userNumber1 = randomNum.nextInt(100);
+                    userNumber2 = randomNum.nextInt(100);
+                    break;
+                }
     }
     private boolean checkOption(int userOption, int otherOption) {
         if (userOption > otherOption) {
